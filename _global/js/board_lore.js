cc662d60ed681f10b62fbe2b59e4445a1bfa3e38
Jordi Capdevila <jordi.capdevila@isern.tv>
2021-07-13 17:32:27 +0200

    Board lore, added "uses tokens" section in summary of powers

--- _global/js/board_lore.js
+++ _global/js/board_lore.js
@@ -3,25 +3,16 @@ window.onload = function startMain(){
     document.querySelectorAll('board')[0].innerHTML = replaceIcon(html);
     adjustComplexityValue();
     createPowerProperties();
+    addUsesTokens();
 }
 
 function adjustComplexityValue() {
     var complexityValue = document.getElementsByTagName("complexity-value")[0].getAttribute("value");
-    console.log(complexityValue);
     //add 45px for each value
     var basePixels = 120;
     var addedPixels = (complexityValue*45);
     var totalPixels = basePixels+addedPixels+"px";
-    console.log(totalPixels);
     document.getElementsByTagName("complexity-value")[0].style.width = totalPixels;
-}
-
-function adjustPowerValue(tag, value) {
-    var basePixels = 15;
-    var addedPixels = (value*15);
-    var totalPixels = basePixels+addedPixels+"px";
-    console.log(totalPixels);
-    tag.style.height = totalPixels;
 }
 
 function createPowerProperties(){
@@ -43,4 +34,33 @@ function createPowerProperties(){
     fearTag.style.height = (fearValue * 14) + 'px';
     defenseTag.style.height = (defenseValue * 14) + 'px';
     utilityTag.style.height = (utilityValue * 14) + 'px';
+}
+
+function addUsesTokens() {
+    var usesTokensTag = document.getElementsByTagName("uses-tokens")[0];
+    if (usesTokensTag) {
+        var border = document.createElement("uses-tokens-border");
+        usesTokensTag.parentNode.insertBefore(border, usesTokensTag);
+
+        var usesHTML = "<table><tr><td colspan='100%'>USES<td></tr><tr>";
+        var tokenValues = usesTokensTag.getAttribute("values").split(",");
+
+        for (i = 0; i < tokenValues.length; i++) {
+            if (i == 2)
+                usesHTML += "</tr><tr>";
+
+            // 3 token elements
+            if (tokenValues.length == 3 && i == 2)
+                usesHTML += "<td colspan='2'>";
+            else
+                usesHTML += "<td>";
+
+            usesHTML += `${replaceIcon('{' + tokenValues[i] + '}')}</td>`
+        }
+        usesHTML += "</tr></table>";
+
+        document.getElementsByTagName("uses-tokens")[0].removeAttribute("values");
+
+        document.getElementsByTagName("uses-tokens")[0].innerHTML = usesHTML;
+    }
 }
