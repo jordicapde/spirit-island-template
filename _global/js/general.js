8bfdbeb497f461b34d88bdda5189edefed232ac1
Jordi Capdevila <jordi.capdevila@isern.tv>
2021-08-02 09:36:20 +0200

    Added blurred css class and generic control in general.js

--- _global/js/general.js
+++ _global/js/general.js
@@ -4,13 +4,14 @@ function replaceIcon(html)
 
   var regEx = new RegExp('(\\{[^\\}]*\\})', "ig");
   var matchs = result.match(regEx);
-  for(var match of (matchs || []))
-  {
+  for(var match of (matchs || [])) {
     var iconName = match.replace('{', '').replace('}', '');
-    let iconHtml
-    if(iconName.startsWith('no-')){
+    let iconHtml;
+    if (iconName.startsWith('no-')) {
         iconHtml = `<icon class="no"><icon class="${iconName.substring(3)}"></icon></icon>`;
-    }else{
+    } else if (iconName.startsWith('blurred-')) {
+        iconHtml = `<icon class="${iconName.substring(8)} blurred"></icon>`;
+    } else {
         iconHtml = `<icon class="${iconName}"></icon>`;
     }
     result = result.replace(new RegExp(match, "ig"), iconHtml);
